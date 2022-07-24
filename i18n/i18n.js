2fc152647efc671710afb8cc71e0eaf1b5cee19a
Sofiya Tepikin <sofiya.tepikin@gmail.com>
2022-07-24 09:54:31 +0200

    Prettify i18.js

--- i18n/i18n.js
+++ i18n/i18n.js
@@ -1,15 +1,14 @@
-import i18n from 'i18next';
-import { initReactI18next } from 'react-i18next';
+import i18n from 'i18next'
+import { initReactI18next } from 'react-i18next'
 
 // translation files
-import en from './en.json';
+import en from './en.json'
 
 const resources = {
-    en: { translation: en },
-  };
+  en: { translation: en },
+}
 
-
-  i18n
+i18n
   // pass the i18n instance to react-i18next.
   .use(initReactI18next)
   // init i18next
@@ -21,8 +20,7 @@ const resources = {
 
     interpolation: {
       escapeValue: false, // not needed for react as it escapes by default
-    }
-  });
+    },
+  })
 
-
-export default i18n;
+export default i18n
