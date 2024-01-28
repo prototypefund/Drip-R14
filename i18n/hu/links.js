405c7c567c5f3793ac4d852fba2ad36a3f17920d
summoner <summoner@vivaldi.net>
2024-01-28 13:38:23 +0000

    Upload New File

---
+++ i18n/hu/links.js
@@ -0,0 +1,43 @@
+export default {
+  gitlab: {
+    url: 'https://gitlab.com/bloodyhealth/drip',
+    text: 'GitLab',
+  },
+  email: {
+    url: 'mailto:drip@mailbox.org',
+    text: 'email',
+  },
+  wiki: {
+    url: 'https://gitlab.com/bloodyhealth/drip/wikis/home',
+    text: 'our wiki',
+  },
+  website: {
+    url: 'https://dripapp.org/',
+    text: 'Website',
+  },
+  donate: {
+    url: 'https://ko-fi.com/dripapp',
+    text: 'Donate here',
+  },
+  smashicons: {
+    url: 'https://smashicons.com/',
+    text: 'Smashicons',
+  },
+  pause08: {
+    url: 'https://www.flaticon.com/authors/pause08',
+    text: 'Pause08',
+  },
+  kazachek: {
+    url: 'https://www.flaticon.com/authors/kirill-kazachek',
+    text: 'Kirill Kazachek',
+  },
+  freepik: {
+    url: 'https://www.flaticon.com/authors/freepik',
+    text: 'Freepik',
+  },
+  flaticon: {
+    url: 'https://www.flaticon.com',
+    text: 'Flaticon',
+  },
+}
+
